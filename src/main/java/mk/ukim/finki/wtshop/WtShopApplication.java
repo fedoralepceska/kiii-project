16f978634f0383c3cc79567ea2937de2055ce881
atodorovska <ana5todorovska@gmail.com>
2022-01-09 00:28:35 +0100

    initial commit

---
+++ src/main/java/mk/ukim/finki/wtshop/WtShopApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wtshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtShopApplication.class, args);
+	}
+
+}
