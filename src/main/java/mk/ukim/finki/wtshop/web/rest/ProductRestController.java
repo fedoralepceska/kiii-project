0d4545fbedd1327ce9e2a5b7a0975dc62e76e5f4
atodorovska <ana5todorovska@gmail.com>
2022-01-09 01:30:42 +0100

    Testing properties

---
+++ src/main/java/mk/ukim/finki/wtshop/web/rest/ProductRestController.java
@@ -0,0 +1,10 @@
+package mk.ukim.finki.wtshop.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductRestController {
+
+}
